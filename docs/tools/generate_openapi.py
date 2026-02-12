2ff19dbe9bcf3a68b3bc5f37cd73e105dc400195
isit12 <soliddevwork@gmail.com>
2026-02-12 16:16:31 +0200

    Remove path manipulation antipatterns (#1064) (server.mdx, generate_openapi.py)

--- docs/tools/generate_openapi.py
+++ docs/tools/generate_openapi.py
@@ -1,10 +1,5 @@
 
 import json
-import sys
-import os
-
-# Add src to sys.path so we can import memmachine
-sys.path.insert(0, os.path.join(os.getcwd(), "src"))
 
 from fastapi import FastAPI
 from memmachine.server.api_v2.router import load_v2_api_router
@@ -12,7 +7,7 @@ from memmachine.server.api_v2.router import load_v2_api_router
 def generate_openapi():
     app = FastAPI(servers=[{"url": "https://localhost:8080"}])
     load_v2_api_router(app)
-    
+
     openapi_schema = app.openapi()
     print(json.dumps(openapi_schema, indent=2))
 
